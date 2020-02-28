1db26238020d03be30a253600d93741f08382b3d
hakbum <hhbhong@gmail.com>
2020-02-28 16:22:33 +0900

    Add prisma and docker compose

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,22 @@ import resolvers from "./resolvers";
 import userModel from "./models/userModel";
 import postModel from "./models/postModel";
 
+// prisma
+import { prisma } from "./generated/prisma-client";
+
+// A `main` function so that we can use async/await
+async function main() {
+  // Create a new user called `Alice`
+  const newUser = await prisma.createUser({ name: "Alice" });
+  console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`);
+
+  // Read all users from the database and print them to the console
+  const allUsers = await prisma.users();
+  console.log(allUsers);
+}
+
+main().catch(e => console.error(e));
+
 const app = express();
 app.use("*", cors());
 app.use(compression());
